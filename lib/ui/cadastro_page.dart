1f6c716185599c39a2a528a11d76552c1835a436
Danilo Araujo de Souza <danilo.souza@edu.unipar.br>
2020-03-14 11:43:14 -0300

    Adicionado button em cadastro de livros

--- lib/ui/cadastro_page.dart
+++ lib/ui/cadastro_page.dart
@@ -28,6 +28,10 @@ class _CadastroPageState extends State<CadastroPage> {
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
+            criarCampo("Nome", nomeController, TextInputType.text),
+            criarCampo("Editora", editoraController, TextInputType.text),
+            criarCampo("Ano", anoController, TextInputType.number),
+            criarBotao("Salvar", Colors.green, Icons.save, Colors.white, salvarLivro)
           ],
         ),
       ),
@@ -43,11 +47,33 @@ class _CadastroPageState extends State<CadastroPage> {
         keyboardType: ty,
         style: TextStyle(color: Colors.black, fontSize: 16),
         decoration: InputDecoration(
-          labelText: texto,
-          labelStyle: TextStyle(color: Colors.black),
-          border: OutlineInputBorder()
+            labelText: texto,
+            labelStyle: TextStyle(color: Colors.black),
+            border: OutlineInputBorder()
         ),
       ),
     );
+  }
+
+  Widget criarBotao(String texto, Color corBotao, IconData icone, Color corTexto, Function f){
+    return Padding(
+      padding: EdgeInsets.fromLTRB(60, 10, 60, 0),
+      child: RaisedButton(
+        color: corBotao,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(icone, size: 26, color: corTexto,),
+            Text(texto, style: TextStyle(
+              fontSize: 22, color: corTexto
+            ),)
+          ],
+        ), onPressed: f,
+      ),
+    );
+  }
+
+  void salvarLivro(){
+
   }
 }
