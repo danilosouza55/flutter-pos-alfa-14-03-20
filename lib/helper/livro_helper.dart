8adfe29996ffc93c7a9fa351b42c248aa75e9aae
Danilo Araujo de Souza <danilo.souza@edu.unipar.br>
2020-03-14 08:51:25 -0300

    inicial

---
+++ lib/helper/livro_helper.dart
@@ -0,0 +1,8 @@
+import 'package:sqflite/sql.dart';
+
+final String nomeBancoDados = "banco_dados_db";
+final String livroTabela = "TbLivro";
+final String codigoColumn = "codigo";
+final String nomeColmn = "nome";
+final String editoraColumn = "editora";
+final String anoColumn = "ano";
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:aula140320/ui/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(MaterialApp(
+    title: "Meu App",
+    debugShowCheckedModeBanner: false,
+    home: new HomePage(),
+  ));
+}
